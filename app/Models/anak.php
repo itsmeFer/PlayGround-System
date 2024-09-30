c7b7e45deb82ca0ea12daa81fb792c19df8ff933
Ferdinand <ferdinandsianturi28@gmail.com>
2024-09-30 22:38:08 +0700

    create

--- app/Models/anak.php
+++ app/Models/anak.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class anak extends Model
+class Anak extends Model
 {
     use HasFactory;
+
+    // Menambahkan fillable property
+    protected $fillable = [
+        'nama',
+        'umur',
+        'alamat',
+        'orang_tua',
+        'kontak_orang_tua',
+        'member',
+    ];
 }
